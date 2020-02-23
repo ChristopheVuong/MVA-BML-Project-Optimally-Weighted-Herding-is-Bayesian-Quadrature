263a9c0955c3a5bf45f7bc1ca324f79fa9a3d5a6
Clément Bonet <clementbonet@hotmail.fr>
2020-02-23 21:11:56 +0100

    Inversion partitioned Matrix SBQ

--- SBQ.py
+++ SBQ.py
@@ -27,6 +27,40 @@ def scoreSBQ(samples,kernel,gm,new_samples,gram,z,k):
     
     return scores
 
+def scoreSBQ(samples,kernel,gm,new_samples,gram,z,k):
+    """
+        With inversion of partitioned matrix formula
+        http://www.gaussianprocess.org/gpml/chapters/RWA.pdf p3 (201)
+    """
+    
+    g = gram.copy()
+    newZ = z.copy()
+    zz = np.array([newZ for l in range(len(new_samples))])
+    
+    for l in range(len(gm.means)):
+        zz[:,k] += gm.weights[l]*E_Gaussian(new_samples,kernel.covariance,gm.means[l],gm.covariances[l])
+        
+    scores = []
+    K1_1 = np.linalg.inv(g)
+    for l,sample in enumerate(new_samples):
+        update(k,g,sample,kernel,gm,samples)
+        
+        if(k==0):
+            K_tilde = np.linalg.inv(g)
+        else:
+
+            k_tilde = 1/(1-g[k,:k]@K1_1[:k,:k]@g[:k,k])
+            K_tilde = np.eye(g.shape[0])
+            
+            K_tilde[:k,:k] = K1_1[:k,:k]+k_tilde*K1_1[:k,:k]@g[:k,k].reshape(-1,1)@g[k,:k].reshape(1,-1)@K1_1[:k,:k]
+            K_tilde[k,:k] = -k_tilde*g[k,:k].reshape(1,-1)@K1_1[:k,:k]
+            K_tilde[:k,k] = K_tilde[k,:k].T
+            K_tilde[k,k] = k_tilde
+        
+        scores.append(zz[l,:].T@K_tilde@zz[l,:])
+    
+    return scores
+
 def generate_SBQ(kernel,gm,area,samples,gram,z,k,num_queries=10000):
     d = area.shape[0]
     new_samples = area[None, 0] + \
